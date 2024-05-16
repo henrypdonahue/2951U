73358446724969ef103a1c01d89aecbf9aaec37b
Henry Donahue <henrydonahue@henrys-mbp-8.mynetworksettings.com>
2024-05-15 22:18:51 -0400

    unreal users cant use pacia

---
+++ new/sum-pac.s
@@ -0,0 +1,24 @@
+.align 2
+.text
+.global _main
+//compute sum recursively of all number up to N
+//where N is passed in as x0
+sum:
+    cmp  x0,1
+    b.le sum_exit
+    // pacia lr,x28
+    stp   x0,lr,[sp,#-16]!
+    add   x0,x0,-1
+    bl    sum
+    ldp   x1,lr,[sp],#16
+    // autia lr,x28            //"decrypt" the return addr
+    add   x0,x0,x1
+sum_exit:   ret
+
+_main:
+    stp lr,x28,[sp,#-16]!
+    mov x28,0
+    mov x0,5
+    bl  sum
+    ldp lr,x28,[sp],#16
+    ret
---
+++ test.s
@@ -0,0 +1,22 @@
+.align 2
+.text
+.global _main
+//compute sum recursively of all number up to N
+//where N is passed in as x0
+sum:
+    cmp  x0,1
+    b.le sum_exit
+    stp  x0,lr,[sp,#-16]!
+    add  x0,x0,-1
+    bl   sum
+    ldp  x1,lr,[sp],#16
+    add  x0,x0,x1
+sum_exit:   ret
+
+
+_main:
+    str lr,[sp,#-16]!
+    mov x0,9
+    bl  sum
+    ldr lr,[sp],#16
+    ret
